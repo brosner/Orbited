ee3b4082747cae358ebffbb912d76cd7676063f3
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-03-20 15:38:58 +0000

    git-svn-id: http://orbited.org/svn/orbited/trunk@101 fe34127c-c842-0410-aa83-9fc675b9a81c

--- demos/python/livehelp/static/config.js
+++ demos/python/livehelp/static/config.js
@@ -1,6 +1,6 @@
 TOP_DOMAIN = "orbited.org"
 APP_HOSTNAME = "http://orbited.org/livehelp"
-ORBIT_HOSTNAME = "http://orbited.org:8000"
+ORBIT_HOSTNAME = "http://"+document.location.toString().split('/')[2]
 var pieces = document.location.toString().split('?')
 if (pieces.length == 1) {
     CHANNEL = "orbited"
--- demos/python/livehelp/static/lconfig.js
+++ demos/python/livehelp/static/lconfig.js
@@ -1,7 +1,7 @@
 alert("Local Configuration")
 TOP_DOMAIN = "127.0.0.1"
 APP_HOSTNAME = "http://127.0.0.1:8000"
-ORBIT_HOSTNAME = "http://127.0.0.1:8000"
+ORBIT_HOSTNAME = "http://"+document.location.toString().split('/')[2]
 var pieces = document.location.toString().split('?')
 if (pieces.length == 1) {
     CHANNEL = "orbited"
