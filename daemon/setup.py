28837df1d5ea22b6d578c7da51148c4aeed79159
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2009-01-21 06:06:11 +0000

    svn/setuptools conflict (NameError) addressed
    git-svn-id: http://orbited.org/svn/orbited/trunk@561 fe34127c-c842-0410-aa83-9fc675b9a81c

--- daemon/setup.py
+++ daemon/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-import os, sys
+import os
 
 static_types = [
     '*.js', 
@@ -14,40 +14,41 @@ static_types = [
     '*.template'
 ]
 
-_install_requires = [ "demjson", "morbid" ]
-
-#if sys.platform != "win32":
-#    _install_requires.append("Twisted")
-
-setup(
-    name='orbited',
-    version='0.7.2',
-    author='Michael Carter',
-    author_email='CarterMichael@gmail.com',
-    url='http://www.orbited.org',
-    download_url='http://www.orbited.org/download',
-    license='MIT License',
-    description='A browser(javascript)->tcp bridge; Comet is used to emulate TCP connections in the browser; Allows you to connect a web browser directly to an IRC or XMPP server, for instance.',
-    long_description='',
-    packages= find_packages(),
-    package_data = {'': reduce(list.__add__, [ '.svn' not in d and [ os.path.join(d[len('orbited')+1:], e) for e in
-            static_types ] or [] for (d, s, f) in os.walk(os.path.join('orbited', 'static'))
-        ]) },
-    zip_safe = False,
-    install_requires = _install_requires,
-    entry_points = '''    
-        [console_scripts]
-        orbited = orbited.start:main
-        orbitedctl = orbited.control:main
-    ''',
-    
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],        
-)
+try:
+    setup(
+        name = 'orbited',
+        version = '0.7.2',
+        author = 'Michael Carter',
+        author_email = 'CarterMichael@gmail.com',
+        url = 'http://www.orbited.org',
+        download_url = 'http://www.orbited.org/download',
+        license = 'MIT License',
+        description = 'A browser(javascript)->tcp bridge; Comet is used to emulate TCP connections in the browser; Allows you to connect a web browser directly to an IRC or XMPP server, for instance.',
+        long_description = '',
+        packages = find_packages(),
+        package_data = {'': reduce(list.__add__, [ '.svn' not in d and [ os.path.join(d[len('orbited')+1:], e) for e in
+                static_types ] or [] for (d, s, f) in os.walk(os.path.join('orbited', 'static'))
+            ]) },
+        zip_safe = False,
+        install_requires = [ "demjson", "morbid" ],
+        entry_points = '''
+            [console_scripts]
+            orbited = orbited.start:main
+            orbitedctl = orbited.control:main
+        ''',
+        classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
+except NameError:
+    print "\nerror! Orbited NOT installed!"
+    print "There is a known conflict between subversion 1.5 and some old versions of setuptools."
+    print 'To install Orbited, first update setuptools with the command "easy_install -U setuptools". This is recommended.'
+    print "If you don't want the new version of setuptools, you can svn export the checked-out Orbited directory, and run setup.py from there."
+    print "(svn export info: http://svnbook.red-bean.com/en/1.0/re10.html)"
