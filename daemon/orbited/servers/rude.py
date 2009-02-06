1df46238db5d377fb2c8db724f914c0ffc85e25a
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2009-02-06 05:16:30 +0000

    test server changed to close connection immediately
    git-svn-id: http://orbited.org/svn/orbited/trunk@572 fe34127c-c842-0410-aa83-9fc675b9a81c

--- daemon/orbited/servers/rude.py
+++ daemon/orbited/servers/rude.py
@@ -1,5 +1,8 @@
-from twisted.internet import protocol
+from twisted.internet import protocol, reactor
 
 class Rude(protocol.Protocol):
-    def dataReceived(self, data):
+    def __init__(self):
+        reactor.callLater(0.0001, self.hang_up)
+
+    def hang_up(self):
         self.transport.loseConnection()
